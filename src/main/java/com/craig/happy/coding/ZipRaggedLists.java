cf3897bf0b5d6c9b448a36e66ce3510439b9623e
csteele23 <cts3@njit.edu>
2023-03-01 22:53:22 -0500

    made private method to check if object is a list in ZipRaggedLists.java

--- src/main/java/com/craig/happy/coding/ZipRaggedLists.java
+++ src/main/java/com/craig/happy/coding/ZipRaggedLists.java
@@ -8,25 +8,25 @@ public class ZipRaggedLists {
 
     public List<Object> zippedRaggedList(Object object1, Object object2) {
         List<Object> zippedRaggedList = new ArrayList<>();
-        boolean isObject1List = object1 instanceof List;
-        boolean isObject2List = object2 instanceof List;
         if (Objects.isNull(object1) || Objects.isNull(object2)) return zippedRaggedList;
-        if (isObject1List || isObject2List) {
-            List<Object> list = (List<Object>) (isObject1List && isObject2List ?
+        if (isObjectList(object1) || isObjectList(object2)) {
+            List<Object> list = (List<Object>) (isObjectList(object1) && isObjectList(object2) ?
                     ((List<?>) object1).size() <= ((List<?>) object2).size() ?
-                            object1 : object2 : isObject1List ? object1 : object2);
+                            object1 : object2 : isObjectList(object1) ? object1 : object2);
             for (int i = 0; i < list.size(); i++) {
-                Object subObject1 = isObject1List ? ((List<Object>) object1).get(i) : object1;
-                Object subObject2 = isObject2List ? ((List<Object>) object2).get(i) : object2;
-                boolean isSubObject1List = subObject1 instanceof List;
-                boolean isSubObject2List = subObject2 instanceof List;
-                zippedRaggedList.add(isSubObject1List || isSubObject2List ?
+                Object subObject1 = isObjectList(object1) ? ((List<Object>) object1).get(i) : object1;
+                Object subObject2 = isObjectList(object2) ? ((List<Object>) object2).get(i) : object2;
+                zippedRaggedList.add(isObjectList(subObject1) || isObjectList(subObject2) ?
                         zippedRaggedList(subObject1, subObject2) : String.format("(%s,%s)", subObject1, subObject2));
             }
         } else {
             zippedRaggedList.add(String.format("(%s,%s)", object1, object2));
         }
         return zippedRaggedList;
+    }
+
+    private boolean isObjectList(Object object) {
+        return object instanceof List;
     }
 
 }
