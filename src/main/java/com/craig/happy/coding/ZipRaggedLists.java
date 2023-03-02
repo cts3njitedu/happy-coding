91cc84fe265e95fd0c84d2f40ec7216c696bd157
csteele23 <cts3@njit.edu>
2023-03-01 23:28:50 -0500

    create method to get subObject in ZipRaggedLists.java

--- src/main/java/com/craig/happy/coding/ZipRaggedLists.java
+++ src/main/java/com/craig/happy/coding/ZipRaggedLists.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class ZipRaggedLists {
 
+    private final String TUPLE_STRING_FORMAT = "(%s,%s)";
 
     public List<Object> zippedRaggedList(Object object1, Object object2) {
         List<Object> zippedRaggedList = new ArrayList<>();
@@ -13,13 +14,14 @@ public class ZipRaggedLists {
         if (isObjectList(object1) || isObjectList(object2)) {
             int minimumMaxSize = getMinimumMaxSize(object1, object2);
             for (int i = 0; i < minimumMaxSize; i++) {
-                Object subObject1 = isObjectList(object1) ? ((List<Object>) object1).get(i) : object1;
-                Object subObject2 = isObjectList(object2) ? ((List<Object>) object2).get(i) : object2;
+                Object subObject1 = getSubObject(object1, i);
+                Object subObject2 = getSubObject(object2, i);
                 zippedRaggedList.add(isObjectList(subObject1) || isObjectList(subObject2) ?
-                        zippedRaggedList(subObject1, subObject2) : String.format("(%s,%s)", subObject1, subObject2));
+                        zippedRaggedList(subObject1, subObject2)
+                        : String.format(TUPLE_STRING_FORMAT, subObject1, subObject2));
             }
         } else {
-            zippedRaggedList.add(String.format("(%s,%s)", object1, object2));
+            zippedRaggedList.add(String.format(TUPLE_STRING_FORMAT, object1, object2));
         }
         return zippedRaggedList;
     }
@@ -29,6 +31,11 @@ public class ZipRaggedLists {
         return ((List<Object>) ((isObjectList(object1) && isObjectList(object2)) ?
                 ((((List<?>) object1).size() <= ((List<?>) object2).size()) ?
                         object1 : object2) : (isObjectList(object1) ? object1 : object2))).size();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Object getSubObject(Object object, int i) {
+        return isObjectList(object) ? ((List<Object>) object).get(i) : object;
     }
 
     private boolean isObjectList(Object object) {
