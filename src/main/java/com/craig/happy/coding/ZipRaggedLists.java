d7f6fd0863363ca5a19dd35a6a2eab4bd2e10bf5
csteele23 <cts3@njit.edu>
2023-03-01 23:00:00 -0500

    move logic to get min max size to a separate method in ZipRaggedLists.java

--- src/main/java/com/craig/happy/coding/ZipRaggedLists.java
+++ src/main/java/com/craig/happy/coding/ZipRaggedLists.java
@@ -6,14 +6,13 @@ import java.util.Objects;
 
 public class ZipRaggedLists {
 
+
     public List<Object> zippedRaggedList(Object object1, Object object2) {
         List<Object> zippedRaggedList = new ArrayList<>();
         if (Objects.isNull(object1) || Objects.isNull(object2)) return zippedRaggedList;
         if (isObjectList(object1) || isObjectList(object2)) {
-            List<Object> list = (List<Object>) (isObjectList(object1) && isObjectList(object2) ?
-                    ((List<?>) object1).size() <= ((List<?>) object2).size() ?
-                            object1 : object2 : isObjectList(object1) ? object1 : object2);
-            for (int i = 0; i < list.size(); i++) {
+            int minimumMaxSize = getMinimumMaxSize(object1, object2);
+            for (int i = 0; i < minimumMaxSize; i++) {
                 Object subObject1 = isObjectList(object1) ? ((List<Object>) object1).get(i) : object1;
                 Object subObject2 = isObjectList(object2) ? ((List<Object>) object2).get(i) : object2;
                 zippedRaggedList.add(isObjectList(subObject1) || isObjectList(subObject2) ?
@@ -23,6 +22,13 @@ public class ZipRaggedLists {
             zippedRaggedList.add(String.format("(%s,%s)", object1, object2));
         }
         return zippedRaggedList;
+    }
+
+    @SuppressWarnings("unchecked")
+    private int getMinimumMaxSize(Object object1, Object object2) {
+        return ((List<Object>) ((isObjectList(object1) && isObjectList(object2)) ?
+                ((((List<?>) object1).size() <= ((List<?>) object2).size()) ?
+                        object1 : object2) : (isObjectList(object1) ? object1 : object2))).size();
     }
 
     private boolean isObjectList(Object object) {
