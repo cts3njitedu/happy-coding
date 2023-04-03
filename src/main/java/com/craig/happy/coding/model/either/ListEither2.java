c92171105b27f7a396fcfe678849a3e097740410
csteele23 <cts3@njit.edu>
2023-04-03 15:02:42 -0400

    added another valid item and fixed formatting

--- src/main/java/com/craig/happy/coding/model/either/ListEither2.java
+++ src/main/java/com/craig/happy/coding/model/either/ListEither2.java
@@ -7,74 +7,76 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ListEither2<R>
-        extends Either<List<ListEither2<R>>, R> {
+    extends Either<List<ListEither2<R>>, R> {
 
-    private static final Set<Class<?>> validItems = Set.of(Number.class, Character.class, String.class);
+  private static final Set<Class<?>> validItems = Set.of(Number.class, Character.class,
+      String.class, Boolean.class);
 
-    public ListEither2(List<ListEither2<R>> l) {
-        super(l, null);
+  public ListEither2(List<ListEither2<R>> l) {
+    super(l, null);
+  }
+
+  public ListEither2(R r) {
+    super(null, r);
+  }
+
+  public ListEither2() {
+    super(null, null);
+  }
+
+  public static <G> ListEither2<G> of() {
+    return new ListEither2<>();
+  }
+
+  public static <G> ListEither2<G> ofItem(G g) {
+    boolean isValid = validItems.stream()
+        .anyMatch(cls -> cls.isInstance(g));
+    if (isValid) {
+      return new ListEither2<>(g);
+    } else {
+      throw new IllegalArgumentException(
+          String.format("Element must be one of these class types %s, " +
+              "but element has a class type of %s", validItems.stream()
+              .map(Class::getName)
+              .collect(Collectors.joining(",")), g.getClass()));
     }
 
-    public ListEither2(R r) {
-        super(null, r);
-    }
+  }
 
-    public ListEither2() {
-        super(null, null);
-    }
+  public static <G> ListEither2<G> ofList(List<ListEither2<G>> l) {
+    return new ListEither2<>(l);
+  }
 
-    public static <G> ListEither2<G> of() {
-        return new ListEither2<>();
-    }
+  @SafeVarargs
+  public static <G> ListEither2<G> ofList(ListEither2<G> g, ListEither2<G>... v) {
+    List<ListEither2<G>> c = new ArrayList<>();
+    c.add(g);
+    c.addAll(List.of(v));
+    return new ListEither2<>(c);
+  }
 
-    public static <G> ListEither2<G> ofItem(G g) {
-        boolean isValid = validItems.stream()
-                .anyMatch(cls -> cls.isInstance(g));
-        if (isValid) {
-            return new ListEither2<>(g);
-        } else {
-            throw new IllegalArgumentException(String.format("Element must be one of these class types %s, " +
-                    "but element has a class type of %s", validItems.stream()
-                    .map(Class::getName)
-                    .collect(Collectors.joining(",")), g.getClass()));
-        }
+  public boolean isList() {
+    return super.isLeft();
+  }
 
-    }
+  public boolean isItem() {
+    return super.isRight();
+  }
 
-    public static <G> ListEither2<G> ofList(List<ListEither2<G>> l) {
-        return new ListEither2<>(l);
-    }
+  public boolean isEmpty() {
+    return (Objects.isNull(super.getLeft()) || super.getLeft().isEmpty())
+        && Objects.isNull(super.getRight());
+  }
 
-    @SafeVarargs
-    public static <G> ListEither2<G> ofList(ListEither2<G> g, ListEither2<G>... v) {
-        List<ListEither2<G>> c = new ArrayList<>();
-        c.add(g);
-        c.addAll(List.of(v));
-        return new ListEither2<>(c);
-    }
+  public ListEither2<R> getListEither() {
+    return this;
+  }
 
-    public boolean isList() {
-        return super.isLeft();
-    }
+  public List<ListEither2<R>> getList() {
+    return super.getLeft();
+  }
 
-    public boolean isItem() {
-        return super.isRight();
-    }
-
-    public boolean isEmpty() {
-        return (Objects.isNull(super.getLeft()) || super.getLeft().isEmpty())
-                && Objects.isNull(super.getRight());
-    }
-
-    public ListEither2<R> getListEither() {
-        return this;
-    }
-
-    public List<ListEither2<R>> getList() {
-        return super.getLeft();
-    }
-
-    public Object getItem() {
-        return super.getRight();
-    }
+  public Object getItem() {
+    return super.getRight();
+  }
 }
