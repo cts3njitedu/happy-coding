2e4904cf6105aa656bba9b024049ca33c47f99e0
csteele23 <cts3@njit.edu>
2023-05-13 18:55:17 -0400

    remove unnecessary constants in EnumerateFreePolyominoesTest.java

--- src/test/java/com/craig/happy/coding/EnumerateFreePolyominoesTest.java
+++ src/test/java/com/craig/happy/coding/EnumerateFreePolyominoesTest.java
@@ -11,16 +11,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class EnumerateFreePolyominoesTest {
 
-  public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_BLACK = "\u001B[30m";
-  public static final String ANSI_RED = "\u001B[31m";
-  public static final String ANSI_GREEN = "\u001B[32m";
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_BLUE = "\u001B[34m";
-  public static final String ANSI_PURPLE = "\u001B[35m";
-  public static final String ANSI_CYAN = "\u001B[36m";
-  public static final String ANSI_WHITE = "\u001B[37m";
-
   private EnumerateFreePolyominoes enumerateFreePolyominoes = new EnumerateFreePolyominoes();
 
   private static Stream<Arguments> enumerateFreePolyominoesCases() {
