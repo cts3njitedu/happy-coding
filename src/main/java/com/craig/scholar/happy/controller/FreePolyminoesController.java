c7d07e8e0be0f131f7ac51975dac63434817f14c
csteele23 <cts3@njit.edu>
2023-06-18 16:22:11 -0400

    use v5 of enumerate free polys in FreePolyminoesController.java

--- src/main/java/com/craig/scholar/happy/controller/FreePolyminoesController.java
+++ src/main/java/com/craig/scholar/happy/controller/FreePolyminoesController.java
@@ -5,7 +5,11 @@ import com.craig.scholar.happy.service.codeexchange.EnumerateFreePolyominoes;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/polyominoes")
@@ -20,7 +24,7 @@ public class FreePolyminoesController {
     public @ResponseBody FreePolyominoesResponse enumerate(@PathVariable Integer n) {
         log.info("Enumerate polyominoes with {} blocks", n);
         return FreePolyominoesResponse.builder()
-                .enumeratedPolyominoes(enumerateFreePolyominoes.enumerateFreePolyominoes(n))
+            .polys(enumerateFreePolyominoes.enumerateFreePolyominoesV5(n))
                 .build();
     }
 
