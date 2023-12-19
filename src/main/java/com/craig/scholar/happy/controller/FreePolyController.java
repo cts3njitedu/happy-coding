c7cd2a2da730bc440ff17fca2afb4ef7e39936ec
csteele23 <cts3@njit.edu>
2023-12-18 22:35:35 -0500

    use enumerateFreePolyService and changed name to FreePolyController.java

--- src/main/java/com/craig/scholar/happy/controller/FreePolyController.java
+++ src/main/java/com/craig/scholar/happy/controller/FreePolyController.java
@@ -1,7 +1,7 @@
 package com.craig.scholar.happy.controller;
 
 import com.craig.scholar.happy.model.FreePolyominoesResponse;
-import com.craig.scholar.happy.service.codeexchange.EnumerateFreePolyominoes;
+import com.craig.scholar.happy.service.codeexchange.freepoly.EnumerateFreePolyServiceImpl;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,16 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/polyominoes")
 @Slf4j
 @RequiredArgsConstructor
-public class FreePolyminoesController {
+public class FreePolyController {
 
     @NonNull
-    private final EnumerateFreePolyominoes enumerateFreePolyominoes;
+    private final EnumerateFreePolyServiceImpl enumerateFreePolyService;
 
     @GetMapping("/enumerate/{n}")
     public @ResponseBody FreePolyominoesResponse enumerate(@PathVariable Integer n) {
         log.info("Enumerate polyominoes with {} blocks", n);
         return FreePolyominoesResponse.builder()
-            .polys(enumerateFreePolyominoes.enumerateFreePolyominoesV5(n))
+            .polys(enumerateFreePolyService.enumerate(n))
                 .build();
     }
 
