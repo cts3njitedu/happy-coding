87eb3bd837f773d6afaf7ef0be62fce19d6f3180
csteele23 <cts3@njit.edu>
2023-12-18 23:32:09 -0500

    change url to begin with poly in FreePolyController.java

--- src/main/java/com/craig/scholar/happy/controller/FreePolyController.java
+++ src/main/java/com/craig/scholar/happy/controller/FreePolyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/polyominoes")
+@RequestMapping("/poly")
 @Slf4j
 @RequiredArgsConstructor
 public class FreePolyController {
