0af057f770fa0e5f72d45b62fb71d2238035ad37
csteele23 <cts3@njit.edu>
2023-12-21 15:28:21 -0500

    Added logic to display polyominoes

--- frontend/src/Poly.tsx
+++ frontend/src/Poly.tsx
@@ -7,6 +7,8 @@ type State = {
     numberOfBlocks: string;
     svgWidth: string;
     svgHeight: string;
+    freePolys: number[][][];
+    blockSize: string;
 };
 
 const POLY_ENDPOINT: string = 'http://localhost:8080/poly/enumerate/';
@@ -16,7 +18,9 @@ class Poly extends React.Component<Props, State> {
     state = {
         numberOfBlocks: "",
         svgWidth: "100",
-        svgHeight: "100"
+        svgHeight: "100",
+        freePolys: [],
+        blockSize: "50"
     }
     handleChange = (e: FormEvent<HTMLInputElement>): void => {
         this.setState({ numberOfBlocks: e.currentTarget.value });
@@ -35,13 +39,41 @@ class Poly extends React.Component<Props, State> {
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
-                let size = 100 * parseInt(this.state.numberOfBlocks);
+                this.setState({ freePolys: data.polys })
+                let size = parseInt(this.state.blockSize) * (parseInt(this.state.numberOfBlocks) + 1);
                 this.setState({ svgWidth: size.toString() })
                 this.setState({ svgHeight: size.toString() })
             })
             .catch((err) => {
                 console.log(err.message);
             });
+    }
+
+    translateX = (r: number, rowSize: number) => {
+        let res = this.translate(r, rowSize, parseInt(this.state.svgWidth))
+        console.log("Translate X", r, res);
+        return res;
+    }
+
+    translateY = (c: number, colSize: number) => {
+        let res = this.translate(c, colSize, parseInt(this.state.svgHeight))
+        console.log("Translate Y", c, res)
+        return res;
+    }
+
+    translate = (i: number, size: number, dimension: number) => {
+        let blockSize = parseInt(this.state.blockSize);
+        let iMid = Math.floor(size / 2);
+        if (size % 2 != 0) {
+            return (dimension / 2 - blockSize / 2) + (i - iMid) * blockSize;
+        } else {
+            return dimension / 2 + (i - iMid) * blockSize
+        }
+    }
+
+    print = (r: number, c: number) => {
+        console.log("row", r, "column", c);
+        return 0;
     }
 
     render() {
@@ -56,7 +88,25 @@ class Poly extends React.Component<Props, State> {
                         <button onClick={this.handleClick}>Enumerate</button>
                     </div>
                     <div className="bodyStyle">
+                        {
+                            this.state.freePolys.map((poly: number[][], index) => (
+                                <div key={index}>
+                                    <svg width={this.state.svgWidth} height={this.state.svgHeight} className="svgStyle">
+                                        {
+                                            poly.map((row: number[], rowIndex) => (
+                                                row.map((col: number, colIndex: number) => (
+                                                    col == 1 && <rect width={this.state.blockSize} height={this.state.blockSize}
+                                                                x={this.translateX(rowIndex, poly.length)} 
+                                                                y={this.translateY(colIndex, row.length)}
+                                                                fill="green" strokeWidth="3" stroke="rgb(0,0,0)" />
+                                                ))
+                                            ))
+                                        }
+                                    </svg>
+                                </div>
 
+                            ))
+                        }
                     </div>
                 </div>
             </>
