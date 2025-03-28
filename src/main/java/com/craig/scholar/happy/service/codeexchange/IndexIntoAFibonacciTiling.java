39039bbb412da5c66144101721e5902766e4191a
cts3njitedu <engscholar15@gmail.com>
2025-03-28 15:11:41 -0400

    made changes to IndexIntoAFibonacciTiling.java and IndexIntoAFibonacciTilingTest.java

--- src/main/java/com/craig/scholar/happy/service/codeexchange/IndexIntoAFibonacciTiling.java
+++ src/main/java/com/craig/scholar/happy/service/codeexchange/IndexIntoAFibonacciTiling.java
@@ -1,10 +1,15 @@
 package com.craig.scholar.happy.service.codeexchange;
 
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+import com.craig.scholar.happy.model.BigPoint;
 import com.craig.scholar.happy.model.Point;
+import java.math.BigInteger;
 
 public class IndexIntoAFibonacciTiling implements HappyCodingV2<Point, Integer> {
 
-  record FibonacciTile(int v, Point min, Point max) {
+  record FibonacciTile(int n, Point min, Point max) {
 
     public FibonacciTile(int v, Point min) {
       this(v, min, new Point(min.x() + (v - 1), min.y() + (v - 1)));
@@ -13,6 +18,25 @@ public class IndexIntoAFibonacciTiling implements HappyCodingV2<Point, Integer> 
     public boolean isIndex(Point point) {
       return point.x() >= min.x() && point.x() <= max.x()
           && point.y() >= min.y() && point.y() <= max.y();
+    }
+  }
+
+  record FibonacciBigTile(BigInteger n, BigPoint min, BigPoint max) {
+
+    public FibonacciBigTile(BigInteger n, BigPoint min) {
+      this(n, min, new BigPoint(min.x().add(n.subtract(ONE)),
+          min.y().add(n.subtract(ONE))));
+    }
+
+    public boolean isIndex(BigPoint point) {
+      return point.x().compareTo(min.x()) >= 0
+          && point.x().compareTo(max.x()) <= 0
+          && point.y().compareTo(min.y()) >= 0
+          && point.y().compareTo(max.y()) <= 0;
+    }
+
+    public boolean isOrigin() {
+      return min.isOrigin() && max().isOrigin();
     }
   }
 
@@ -34,25 +58,45 @@ public class IndexIntoAFibonacciTiling implements HappyCodingV2<Point, Integer> 
 
   @Override
   public Integer execute(Point point) {
-    FibonacciTile t1 = new FibonacciTile(1, new Point(0, 0), new Point(0, 0));
+    FibonacciTile t1 = new FibonacciTile(1, new Point(0, 0));
     FibonacciTile t2 = t1;
-    int d = 0;
+    Direction direction = Direction.RIGHT;
     while (!t2.isIndex(point)) {
-      int v = t1 == t2 ? 1 : t1.v + t2.v;
+      int v = t1 == t2 ? 1 : t1.n + t2.n;
       FibonacciTile h = t2;
-      if (d == 0) {
-        t2 = new FibonacciTile(v, new Point(t2.max.x() + 1, t2.min.y()));
-      } else if (d == 1) {
-        t2 = new FibonacciTile(v, new Point(t1.min.x(), t2.max.y() + 1));
-      } else if (d == 2) {
-        t2 = new FibonacciTile(v, new Point(t2.min.x() - v, t1.min.y()));
-      } else {
-        t2 = new FibonacciTile(v, new Point(t2.min.x(), t2.min.y() - v));
-      }
-      d = (d + 1) % 4;
+      t2 = switch (direction) {
+        case RIGHT -> new FibonacciTile(v, new Point(t2.max.x() + 1, t2.min.y()));
+        case UP -> new FibonacciTile(v, new Point(t1.min.x(), t2.max.y() + 1));
+        case LEFT -> new FibonacciTile(v, new Point(t2.min.x() - v, t1.min.y()));
+        case DOWN -> new FibonacciTile(v, new Point(t2.min.x(), t2.min.y() - v));
+      };
+      direction = direction.getAntiClockwiseDirection();
       t1 = h;
     }
-    return t2.v;
+    return t2.n;
+  }
+
+  public BigInteger execute(BigPoint point) {
+    var smallTile = new FibonacciBigTile(ONE, new BigPoint(ZERO, ZERO));
+    var bigTile = smallTile;
+    Direction direction = Direction.RIGHT;
+    while (!bigTile.isIndex(point)) {
+      var n = bigTile.isOrigin() && smallTile.isOrigin() ? ONE : smallTile.n.add(bigTile.n);
+      var hold = bigTile;
+      bigTile = switch (direction) {
+        case RIGHT ->
+            new FibonacciBigTile(n, new BigPoint(bigTile.max.x().add(ONE), bigTile.min.y()));
+        case UP ->
+            new FibonacciBigTile(n, new BigPoint(smallTile.min.x(), bigTile.max.y().add(ONE)));
+        case LEFT ->
+            new FibonacciBigTile(n, new BigPoint(bigTile.min.x().subtract(n), smallTile.min.y()));
+        case DOWN ->
+            new FibonacciBigTile(n, new BigPoint(bigTile.min.x(), bigTile.min.y().subtract(n)));
+      };
+      direction = direction.getAntiClockwiseDirection();
+      smallTile = hold;
+    }
+    return bigTile.n;
   }
 
 }
--- src/test/java/com/craig/scholar/happy/service/codeexchange/IndexIntoAFibonacciTilingTest.java
+++ src/test/java/com/craig/scholar/happy/service/codeexchange/IndexIntoAFibonacciTilingTest.java
@@ -2,8 +2,11 @@ package com.craig.scholar.happy.service.codeexchange;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.craig.scholar.happy.model.BigPoint;
 import com.craig.scholar.happy.model.Point;
+import java.math.BigInteger;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,5 +34,34 @@ class IndexIntoAFibonacciTilingTest {
   @MethodSource("testCases")
   void execute(int x, int y, int expectedFibonacci) {
     assertThat(indexIntoAFibonacciTiling.execute(new Point(x, y))).isEqualTo(expectedFibonacci);
+  }
+
+  static Stream<Arguments> bigTestCases() {
+    return Stream.of(
+        Arguments.of("0", "0", "1"),
+        Arguments.of("1", "0", "1"),
+        Arguments.of("1", "1", "2"),
+        Arguments.of("-3", "1", "3"),
+        Arguments.of("-3", "-1", "5"),
+        Arguments.of("3", "-2", "8"),
+        Arguments.of("6", "15", "13"),
+        Arguments.of("-4", "-5", "21"),
+        Arguments.of("-4", "-6", "34"),
+        Arguments.of("145", "214", "610")
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("bigTestCases")
+  void executeBig(String x, String y, String expectedFibonacci) {
+    assertThat(indexIntoAFibonacciTiling.execute(
+        new BigPoint(new BigInteger(x), new BigInteger(y)))).isEqualTo(
+        new BigInteger(expectedFibonacci));
+  }
+
+  @Test
+  void executeBigSingle() {
+    System.out.println(indexIntoAFibonacciTiling.execute(
+        new BigPoint(new BigInteger("4351241234"), new BigInteger("743452314"))));
   }
 }
