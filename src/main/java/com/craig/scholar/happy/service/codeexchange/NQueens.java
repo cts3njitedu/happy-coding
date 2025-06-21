89eecc02c5a8b4525aaf14239c319a0c53b29084
cts3njitedu <engscholar15@gmail.com>
2025-06-20 23:34:45 -0400

    rename variables in NQueens.java

--- src/main/java/com/craig/scholar/happy/service/codeexchange/NQueens.java
+++ src/main/java/com/craig/scholar/happy/service/codeexchange/NQueens.java
@@ -12,39 +12,39 @@ public class NQueens implements HappyCodingV2<Integer, List<List<String>>> {
 
   @Override
   public List<List<String>> execute(Integer n) {
-    List<char[]> board = IntStream.range(0, n)
+    List<char[]> chessBoard = IntStream.range(0, n)
         .mapToObj(i -> new char[n])
         .peek(row -> Arrays.fill(row, EMPTY_CELL))
         .toList();
-    return execute(board, 0);
+    return execute(chessBoard, 0);
   }
 
-  private List<List<String>> execute(List<char[]> board, int r) {
+  private List<List<String>> execute(List<char[]> chessBoard, int r) {
     var boards = new ArrayList<List<String>>();
-    if (r == board.size()) {
-      boards.add(board.stream()
+    if (r == chessBoard.size()) {
+      boards.add(chessBoard.stream()
           .map(String::valueOf)
           .toList());
       return boards;
     }
-    var row = board.get(r);
+    var row = chessBoard.get(r);
     for (int c = 0; c < row.length; c++) {
-      if (isValidCell(board, r, c)) {
+      if (isValidCell(chessBoard, r, c)) {
         row[c] = QUEEN;
-        boards.addAll(execute(board, r + 1));
+        boards.addAll(execute(chessBoard, r + 1));
         row[c] = EMPTY_CELL;
       }
     }
     return boards;
   }
 
-  private boolean isValidCell(List<char[]> board, int r, int c) {
+  private boolean isValidCell(List<char[]> chessBoard, int r, int c) {
     int diagonalC = c;
     int antiDiagonalC = c;
     while (r >= 0) {
-      if (board.get(r)[c] == QUEEN
-          || diagonalC >= 0 && board.get(r)[diagonalC--] == QUEEN
-          || antiDiagonalC < board.size() && board.get(r)[antiDiagonalC++] == QUEEN) {
+      if (chessBoard.get(r)[c] == QUEEN
+          || diagonalC >= 0 && chessBoard.get(r)[diagonalC--] == QUEEN
+          || antiDiagonalC < chessBoard.size() && chessBoard.get(r)[antiDiagonalC++] == QUEEN) {
         return false;
       }
       r--;
