74b9c5398b3752fe8895a68273d92290c0eec04e
cts3njitedu <engscholar15@gmail.com>
2025-07-04 14:01:59 -0400

    added SudokuTest.java

---
+++ src/test/java/com/craig/scholar/happy/service/codeexchange/SudokuTest.java
@@ -0,0 +1,77 @@
+package com.craig.scholar.happy.service.codeexchange;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SudokuTest {
+
+  Sudoku sudoku = new Sudoku();
+
+  static Stream<Arguments> testCases() {
+    return Stream.of(
+        Arguments.of(
+            new int[][]{
+                {0, 0, 0, 0},
+                {0, 4, 0, 3},
+                {2, 0, 3, 0},
+                {0, 0, 0, 0}
+            },
+            List.of(
+                new int[][][]{
+                    {
+                        {3, 2, 4, 1},
+                        {1, 4, 2, 3},
+                        {2, 1, 3, 4},
+                        {4, 3, 1, 2}
+                    }
+                }
+            )
+        ),
+        Arguments.of(
+            new int[][]{
+                {0, 5, 8, 0, 7, 0, 0, 0, 0},
+                {0, 6, 0, 0, 2, 0, 0, 4, 0},
+                {0, 0, 0, 0, 0, 0, 1, 3, 7},
+                {6, 0, 0, 0, 0, 2, 0, 0, 0},
+                {2, 0, 0, 1, 0, 4, 0, 0, 3},
+                {1, 8, 0, 0, 0, 0, 9, 0, 0},
+                {0, 4, 7, 0, 0, 9, 3, 5, 0},
+                {0, 1, 0, 3, 0, 6, 0, 0, 8},
+                {9, 0, 0, 0, 5, 0, 0, 2, 0}
+            },
+            List.of(
+                new int[][][]{
+                    {
+                        {3, 5, 8, 4, 7, 1, 2, 6, 9},
+                        {7, 6, 1, 9, 2, 3, 8, 4, 5},
+                        {4, 2, 9, 5, 6, 8, 1, 3, 7},
+                        {6, 9, 3, 7, 8, 2, 5, 1, 4},
+                        {2, 7, 5, 1, 9, 4, 6, 8, 3},
+                        {1, 8, 4, 6, 3, 5, 9, 7, 2},
+                        {8, 4, 7, 2, 1, 9, 3, 5, 6},
+                        {5, 1, 2, 3, 4, 6, 7, 9, 8},
+                        {9, 3, 6, 8, 5, 7, 4, 2, 1}
+                    }
+                }
+            )
+        )
+    );
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void execute(int[][] board, List<int[][]> expectedSolutions) {
+    List<int[][]> solutions = sudoku.execute(board);
+    assertThat(solutions).hasSameSizeAs(expectedSolutions);
+    for (int i = 0; i < solutions.size(); i++) {
+      assertThat(solutions.get(i)).isDeepEqualTo(expectedSolutions.get(i));
+    }
+  }
+
+}
